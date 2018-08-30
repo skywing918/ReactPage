113d9c2dc95d696dd8ff5f84120e553cb8b24efe
v-hezhan <v-hezhan@MDS-HEZHAN-01>
2018-08-30 14:30:15 +0800

    update d3

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
 import { Line, Pie } from 'react-chartjs-2';
 import {
-  Card,
-  CardBody,
-  CardHeader,
-  Col,
-  Row,
+    Card,
+    CardBody,
+    CardHeader,
+    Col,
+    Row,
 } from 'reactstrap';
 import chinadata from './china';
 import { geoMercator, geoPath } from 'd3-geo';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
 
-// Main Chart
+// D3 Map
+function D3Map(props) {
+    const { height, width } = props.size;
+    let centerX = 112;
+    let centerY = 41;
+    if (width > 567 && width < 992) {
+        centerX = 162
+        centerY = 41;
+    } else if (width < 567) {
+        centerX = 222
+        centerY = -27;
+    }
+    const projection = geoMercator().center([centerX, centerY]).scale(width / 2.5);
+    const pathGenerator = geoPath().projection(projection);
+    const countries = chinadata.features
+        .map((d, i) => <path
+            key={'path' + i}
+            d={pathGenerator(d)}
+            className='countries'
+            fill='rgba(25, 133, 172, 1)'
+        />);
+
+    return (<svg>
+        {countries}
+    </svg>)
+};
 
 //Random Numbers
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function getTargetDate(diff) {
@@ -31,52 +56,52 @@ var data3 = [];
 
 
 for (var i = 0; i <= elements; i++) {
-  data1.push(random(50, 200));
-  data2.push(random(80, 100));
-  data3.push(getTargetDate(6-i));
+    data1.push(random(50, 200));
+    data2.push(random(80, 100));
+    data3.push(getTargetDate(6 - i));
 }
 
 const mainChart1 = {
-  labels: data3,
-  datasets: [
-    {
-        label: '最近7天注册趋势',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: data1,
-    }
-  ],
+    labels: data3,
+    datasets: [
+        {
+            label: '最近7天注册趋势',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: data1,
+        }
+    ],
 };
 
 const mainChartOpts1 = {
-  tooltips: {
-    enabled: false,
-    custom: CustomTooltips,
-    intersect: true,
-    mode: 'index',
-    position: 'nearest',
-    callbacks: {
-      labelColor: function(tooltipItem, chart) {
-        return { backgroundColor: chart.data.datasets[tooltipItem.datasetIndex].borderColor }
-      }
-    }
-  },
-  maintainAspectRatio: false,
+    tooltips: {
+        enabled: false,
+        custom: CustomTooltips,
+        intersect: true,
+        mode: 'index',
+        position: 'nearest',
+        callbacks: {
+            labelColor: function (tooltipItem, chart) {
+                return { backgroundColor: chart.data.datasets[tooltipItem.datasetIndex].borderColor }
+            }
+        }
+    },
+    maintainAspectRatio: false,
 };
 
 const mainChart2 = {
@@ -163,117 +188,121 @@ const pie2 = {
         }],
 };
 
-const projection = geoMercator().center([112, 41]).scale(700);
-const pathGenerator = geoPath().projection(projection);
+
 
 class Dashboard extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            size: {
+                width: window.innerWidth,
+                height: window.innerHeight
+            }
+        };
 
-        this.countries = chinadata.features
-            .map((d, i) => <path
-                key={'path' + i}
-                d={pathGenerator(d)}
-                className='countries'
-                fill='rgba(25, 133, 172, 1)'
-            />);
     }
 
-render() {
-    return (
-      <div className="animated fadeIn">
-        <Row>
-          <Col xs="12" sm="6" lg="4">
-            <Card>
-                <CardBody className="clearfix p-3 card-body">
-                    <i className="fa fa-laptop bg-info p-3 font-2xl mr-3 float-left"></i>
-                    <div className="text-muted text-uppercase font-weight-bold font-xs">总终端数</div>
-                    <div className="h5 mb-0 text-primary mt-2">1112</div>
-                </CardBody>
-            </Card>
-          </Col>
+    componentDidMount() {
+        var self = this;
+        window.addEventListener("resize", function (e) {
+            self.setState({ size: { width: window.innerWidth, height: window.innerHeight } });
+        });
+    }
 
-          <Col xs="12" sm="6" lg="4">
-            <Card>
-                <CardBody className="clearfix p-3 card-body">
-                    <i className="fa fa-group bg-info p-3 font-2xl mr-3 float-left"></i>
-                    <div className="text-muted text-uppercase font-weight-bold font-xs">用户量</div>
-                    <div className="h5 mb-0 text-primary mt-2">1001</div>
-                </CardBody>
-            </Card>
-          </Col>
+    render() {
 
-          <Col xs="12" sm="6" lg="4">
-            <Card>
-                <CardBody className="clearfix p-3 card-body">
-                    <i className="fa fa-cogs bg-primary p-3 font-2xl mr-3 float-left"></i>
-                    <div className="text-muted text-uppercase font-weight-bold font-xs">APP绑定终端</div>
-                    <div className="h5 mb-0 text-primary mt-2">503</div>
-                </CardBody>
-            </Card>
-          </Col>
-        </Row>
-            <Row>
-                <Col xs="12" sm="6" lg="8">
-                    <svg width={1000} height={575}>
-                        {this.countries}
-                    </svg>
-                </Col>
-                <Col xs="12" sm="6" lg="4">
-                    <Card>
-                        <CardHeader>
-                            终端使用群体统计
+        return (
+            <div className="animated fadeIn">
+                <Row>
+                    <Col xs="12" sm="6" lg="4">
+                        <Card>
+                            <CardBody className="clearfix p-3 card-body">
+                                <i className="fa fa-laptop bg-info p-3 font-2xl mr-3 float-left"></i>
+                                <div className="text-muted text-uppercase font-weight-bold font-xs">总终端数</div>
+                                <div className="h5 mb-0 text-primary mt-2">1112</div>
+                            </CardBody>
+                        </Card>
+                    </Col>
+
+                    <Col xs="12" sm="6" lg="4">
+                        <Card>
+                            <CardBody className="clearfix p-3 card-body">
+                                <i className="fa fa-group bg-info p-3 font-2xl mr-3 float-left"></i>
+                                <div className="text-muted text-uppercase font-weight-bold font-xs">用户量</div>
+                                <div className="h5 mb-0 text-primary mt-2">1001</div>
+                            </CardBody>
+                        </Card>
+                    </Col>
+
+                    <Col xs="12" sm="6" lg="4">
+                        <Card>
+                            <CardBody className="clearfix p-3 card-body">
+                                <i className="fa fa-cogs bg-primary p-3 font-2xl mr-3 float-left"></i>
+                                <div className="text-muted text-uppercase font-weight-bold font-xs">APP绑定终端</div>
+                                <div className="h5 mb-0 text-primary mt-2">503</div>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs="12" sm="6" lg="8">
+                        <D3Map size={this.state.size} />
+                    </Col>
+                    <Col xs="12" sm="6" lg="4">
+                        <Card>
+                            <CardHeader>
+                                终端使用群体统计
               <div className="card-header-actions">
-                               
-                            </div>
-                        </CardHeader>
-                        <CardBody>
-                            <div className="chart-wrapper">
-                                <Pie data={pie1} />
-                            </div>
-                        </CardBody>
-                    </Card>
-                    <Card>
-                        <CardHeader>
-                            APP转化率
+
+                                </div>
+                            </CardHeader>
+                            <CardBody>
+                                <div className="chart-wrapper">
+                                    <Pie data={pie1} />
+                                </div>
+                            </CardBody>
+                        </Card>
+                        <Card>
+                            <CardHeader>
+                                APP转化率
               <div className="card-header-actions">
-                               50%
+                                    50%
                             </div>
-                        </CardHeader>
-                        <CardBody>
-                            <div className="chart-wrapper">
-                                <Pie data={pie2} />
-                            </div>
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
-        <Row>
-          <Col>
-            <Card>
-              <CardBody>
-                <div className="chart-wrapper" style={{ height: 300 + 'px'}}>
-                  <Line data={mainChart1} options={mainChartOpts1} height={300} />
-                </div>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-            
-        <Row>
-            <Col>
-                <Card>
-                    <CardBody>
-                        <div className="chart-wrapper" style={{ height: 300 + 'px' }}>
-                            <Line data={mainChart2} options={mainChartOpts2} height={300} />
-                        </div>
-                    </CardBody>
-                </Card>
-            </Col>
-        </Row>
-      </div>
-    );
-  }
+                            </CardHeader>
+                            <CardBody>
+                                <div className="chart-wrapper">
+                                    <Pie data={pie2} />
+                                </div>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Card>
+                            <CardBody>
+                                <div className="chart-wrapper" style={{ height: 300 + 'px' }}>
+                                    <Line data={mainChart1} options={mainChartOpts1} height={300} />
+                                </div>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <Card>
+                            <CardBody>
+                                <div className="chart-wrapper" style={{ height: 300 + 'px' }}>
+                                    <Line data={mainChart2} options={mainChartOpts2} height={300} />
+                                </div>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default Dashboard;
